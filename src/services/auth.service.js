dbd84de126933ceb3e2cef3642534a5c001f0802
developerthierry <developerthierry@gmail.com>
2026-04-08 13:27:25 +0000

    Secure API with Arcjet

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -25,7 +25,7 @@ export const comparePassword = async (password, hashedPassword) => {
 export const createUser = async ({name, email, password, role='user'}) => {
     try{
         const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1)
-        if(existingUser.length > 0) throw new Error('User already exists')
+        if(existingUser.length > 0) throw new Error('User with this email already exists')
         const password_hash = await hashPassword(password)
         const [newUser] = await db.insert(users).values({name, email, password:password_hash, role}).returning({id:users.id, name:users.name, email:users.email, role:users.role, created_at:users.created_at})
 
